85f15f95c1b207abcbd68f8cbd64514c0fd6ad6f
supattra0304 <supattra3445@gmail.com>
2023-09-19 00:12:37 +0700

    EJS

--- week9_28.08.66/app.js
+++ week9_28.08.66/app.js
@@ -1,21 +1,21 @@
 // import { engine } from "express-handlebars";
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+// const expressHbs = require("express-handlebars");
 
 const path = require("path");
 const routDir = require("./utilities/path");
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs.engine({
-    layoutsDir: "views/layouts",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
+// app.engine(
+//   "hbs",
+//   expressHbs.engine({
+//     layoutsDir: "views/layouts",
+//     defaultLayout: "main-layout",
+//     extname: "hbs",
+//   })
+// );
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -29,7 +29,7 @@ app.use("/admin", adminData.routes);
 app.use(storeRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { docTitle: "404" });
+  res.status(404).render("404", { docTitle: "404" ,path:'*'});
 });
 
 app.listen(3000);
